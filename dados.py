4da6005d9322a9e7b98684dc12033e32140d1628
Pedro <pedrohenriquefa97@gmail.com>
2025-05-19 22:36:22 -0300

    Implement data reading functions for combustiveis and frentistas from CSV files

---
+++ dados.py
@@ -0,0 +1,45 @@
+'''
+combustivel = {
+    'gasolina': {   
+        'preco': 5.79,
+        'volume': 500
+    },
+    'etanol' : {
+        'preco': 4.07,
+        'volume': 200
+    }
+}
+'''
+
+# frentist = {
+#     'nome' : {
+#     'num_vendas' : 0,
+#     'bonus' : 0
+#     },
+# }
+
+import pandas as pd
+
+def le_combustiveis(arquivo):
+    combustiveis = {}
+    dados_combustiveis = pd.read_csv(arquivo)
+    dados_combustiveis = dados_combustiveis.to_dict('records') # Converte o DataFrame em uma lista de dicionários, records trasforma o DataFrame em uma lista de dicionários
+    for dados in dados_combustiveis:
+        combustiveis[dados['nome']]={
+            'preco' : dados['preco'],
+            'volume' : dados['volume']
+        }
+    return combustiveis
+    
+def le_frentistas(arquivo):
+    frentistas = {}
+    dados_frentistas = pd.read_csv(arquivo)
+    dados_frentistas = dados_frentistas.to_dict('records')
+    for dados in dados_frentistas:
+        frentistas[dados['nome']] = {
+            'num_vendas' : 0,
+            'bonus' : 0
+        }
+    return frentistas
+    
+print(le_frentistas('frentistas.csv'))
