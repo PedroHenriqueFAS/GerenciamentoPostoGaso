520dd53d6b84c84b5007d272eab2311b18d59047
Pedro <pedrohenriquefa97@gmail.com>
2025-05-20 23:26:55 -0300

    Refactor main.py to use 'frentistas' in cadastro_posto and enhance operacao function for better operation handling

--- main.py
+++ main.py
@@ -2,20 +2,48 @@ import posto as pt
 from dados import*
 
 combustiveis = le_combustiveis('combustiveis.csv')
-frentista = le_frentistas('frentistas.csv')
+frentistas = le_frentistas('frentistas.csv')
 
 print('Bem-vindo ao sistema de controle de vendas do posto!')
 
 nomePosto = input('Digite o nome do posto: ')
 salarioFixo = float(input('Digite o salario fixo do frentista: '))
 
-postoDados = pt.cadastro_posto(nomePosto, salarioFixo, combustiveis, frentista)
+postoDados = pt.cadastro_posto(nomePosto, salarioFixo, combustiveis, frentistas)
 
-pt.abastecer('Renan', 'gasolina', 300, postoDados)
+#pt.abastecer('Renan', 'gasolina', 300, postoDados)
 
-while True:
+def operacao(op, postoDados):
+    if op == 1: #abastecer
+        frentista = input('Digite o nome do frentista: ')
+        combustivel = input('Digite o combustivel: ')
+        valor = float(input('Digite o valor que o cliente deseja abastecer: '))
+        pt.abastecer(frentista, combustivel, valor, postoDados)
+        return 'Abastecimento realizado com sucesso!'  
+    elif op == 2:
+        if postoDados['bloqueado']:
+            return 'O posto ja esta bloqueado!'
+        else:
+            postoDados['bloqueado'] = True
+            return 'O posto foi desbloqueado!'
+    elif op == 3:
+        if not postoDados['bloqueado']:
+            return 'O posto ja esta bloqueado!'
+        else:
+            postoDados['bloqueado'] = False
+            return 'O posto foi bloqueado!'
+    elif op == 4:
+        relatorio('relatorio 01',postoDados) 
+
+while True: 
     le_txt('dialogo.txt')
     try: 
         op = int(input('Digite o numero da operação que deseja realizar: '))
     except: 
         continue
+    if op ==5:
+        break
+    
+    resposta = operacao(op, postoDados)
+    print(resposta, postoDados)
+print('Sistema encerrado!')
